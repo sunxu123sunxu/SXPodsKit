8c32f0a5a1f64b685968d63565a0acf86436a64b
孙旭 <sunxucxm@163.com>
2020-06-02 22:42:35 +0800

    1

---
+++ SXPodsKit/A.h
@@ -0,0 +1,17 @@
+//
+//  A.h
+//  fdsafsaf
+//
+//  Created by 孙旭 on 2020/5/30.
+//  Copyright © 2020 孙旭. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface A : NSObject
+-(void)dd;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SXPodsKit/A.m
@@ -0,0 +1,15 @@
+//
+//  A.m
+//  fdsafsaf
+//
+//  Created by 孙旭 on 2020/5/30.
+//  Copyright © 2020 孙旭. All rights reserved.
+//
+
+#import "A.h"
+
+@implementation A
+-(void)dd {
+    NSLog(@"制作成功了");
+}
+@end
